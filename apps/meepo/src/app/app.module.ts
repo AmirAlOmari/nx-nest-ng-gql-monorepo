85097db7d29453c18639c30301cbb2dd35387071
Amir Al-Omari <amir.al.omari.work@gmail.com>
2020-05-22 13:21:13 +0200

    add explicit data-access as explicit dep of meepo

--- apps/meepo/src/app/app.module.ts
+++ apps/meepo/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { DataAccessModule } from "@linkedout/data-access";
+
 import { GraphQLModule } from './graphql.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, GraphQLModule],
+  imports: [BrowserModule, HttpClientModule, GraphQLModule, DataAccessModule],
   providers: [],
   bootstrap: [AppComponent]
 })
