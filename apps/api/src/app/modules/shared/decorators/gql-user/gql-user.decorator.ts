6d8748af4959a6021575a060b06c164699baba3a
Amir Al-Omari <amir.al.omari.work@gmail.com>
2020-05-22 16:07:57 +0200

    remove gql-user.decorator workaround

--- apps/api/src/app/modules/shared/decorators/gql-user/gql-user.decorator.ts
+++ apps/api/src/app/modules/shared/decorators/gql-user/gql-user.decorator.ts
@@ -6,15 +6,7 @@ import { User } from '../../../users/models/user/user.model';
 
 export const GqlUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
-    let user: DocumentType<User>;
-
-    if (Array.isArray(context)) {
-      const contextElWithReq = context.find(ce => ce?.req);
-
-      user = contextElWithReq.req.user;
-    } else {
-      user = GqlExecutionContext.create(context).getContext().req.user;
-    }
+    const user: DocumentType<User> = GqlExecutionContext.create(context).getContext().req.user;
 
     if (!user) {
       throw new Error('Unexpected');
