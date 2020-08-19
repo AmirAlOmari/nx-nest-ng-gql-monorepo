cdda602fc5372116eedba418213d3e72eab184ca
Amir Al-Omari <amir.al.omari.work@gmail.com>
2020-08-19 13:58:05 +0200

    build(frontend): add linkedout and linkedout-production confs

---
+++ apps/frontend/src/environments/environment.linkedout.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.interface';
+
+export const environment: Environment = {
+  production: true,
+
+  serverUrl: 'https://api.linkedout.amir-al-omari.dev',
+};
