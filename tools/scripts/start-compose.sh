7b77a422b4e61fd1af672cc43d1483d9f03e4ecd
Amir Al-Omari <amir.al.omari.work@gmail.com>
2020-12-08 17:33:45 +0100

    chore(todo): split changes

--- tools/scripts/start-compose.sh
+++ tools/scripts/start-compose.sh
@@ -5,4 +5,12 @@ COMMIT=$(git log HEAD -n 1 --pretty=format:"%H" | cut -c1-8)
 TAG=${1:-$COMMIT}
 IMAGE=${2:-"linkedout"}
 
-TAG=${TAG} IMAGE=${IMAGE} docker-compose -f ./docker-compose.local.yml up
+DOCKER_COMPOSE_ALIAS="TAG=${TAG} IMAGE=${IMAGE} docker-compose -f ./docker-compose.local.yml"
+
+echo "Docker compose alias: ${DOCKER_COMPOSE_ALIAS}"
+
+echo "${DOCKER_COMPOSE_ALIAS} \$@" >dc_alias.sh
+
+chmod +x dc_alias.sh
+
+./dc_alias.sh up
